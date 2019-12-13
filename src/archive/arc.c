dca785273c801e8aea1f326bba0f535ea78ae89d
shredz <shredz@centrum.cz>
2019-12-13 14:51:57 +0100

    add ARC file validation

--- src/archive/arc.c
+++ src/archive/arc.c
@@ -7,7 +7,17 @@ void A_ReadFile(const char *path)
 {
     FILE *handle = fopen(path, "rb");
 
+    if (handle == NULL)
+    {
+        I_Error("A_ReadFile(): failed to open file");
+    }
+
     fread(&header, sizeof(arc_header_t), 1, handle);
+
+    if (strncmp(header.id, "ARCH", 4))
+    {
+        I_Error("A_ReadFile(): invalid file format");
+    }
 
     fseek(handle, header.directory, SEEK_SET);
 
