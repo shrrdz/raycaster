bd469114e330d6b93ac09cb874fc02e78d85334a
shredz <shredz@centrum.cz>
2019-12-15 19:13:34 +0100

    add bounds checking to column rendering

--- src/render/render.c
+++ src/render/render.c
@@ -2,6 +2,11 @@
 
 void R_RenderColumn(int x, int start, int end, int color)
 {
+    if (x < 0 || x >= video.width || start < 0 || end >= video.height)
+    {
+        I_Error("R_RenderColumn(): from %d to %d at x: %d", start, end, x);
+    }
+
     for (int y = start; y <= end; y++)
     {
         video.color_buffer[y * video.width + x] = color;
